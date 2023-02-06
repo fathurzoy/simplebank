ac3ffb1d91d4befd04d9d6c508c56539d64a8527
fathur rahman fauzan <fathurrahmanf123@gmail.com>
2023-02-06 15:26:50 +0700

    6-db-transaction

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -19,16 +19,11 @@ var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 	var err error
-	// config, err := util.LoadConfig("../..")
-	testDB, err := sql.Open(dbDriver, dbSource)
-	if err != nil {
-		log.Fatal("cannot load config:", err)
-	}
+	testDB, err = sql.Open(dbDriver, dbSource)
 
-	// testDB, err = sql.Open(config.DBDriver, config.DBSource)
-	// if err != nil {
-	// 	log.Fatal("cannot connect to db:", err)
-	// }
+	if err != nil {
+		log.Fatal("cannot conncet to db", err)
+	}
 
 	testQueries = New(testDB)
 
--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -12,10 +12,10 @@ type Store struct {
 	db *sql.DB
 }
 
-// NewStore creates a new store
+// NewStore creates a new Store
 func NewStore(db *sql.DB) *Store {
 	return &Store{
-		db: db,
+		db:      db,
 		Queries: New(db),
 	}
 }
@@ -23,7 +23,7 @@ func NewStore(db *sql.DB) *Store {
 // execTx executes a function within a database transaction
 func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.BeginTx(ctx, nil)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
@@ -31,14 +31,13 @@ func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 	err = fn(q)
 	if err != nil {
 		if rbErr := tx.Rollback(); rbErr != nil {
-			return fmt.Errorf("tx err: %v, rb err:%v", err, rbErr)
+			return fmt.Errorf("tx error: %v, rb error: %v", err, rbErr)
 		}
 		return err
 	}
 
 	return tx.Commit()
 }
-
 
 // TransferTxParams contains the input parameters of the transfer transaction
 type TransferTxParams struct {
@@ -57,7 +56,7 @@ type TransferTxResult struct {
 }
 
 // TransferTx performs a money transfer from one account to the other.
-// It creates the transfer, add account entries, and update accounts' balance within a database transaction
+// It creates a transfer record, and account entries, and update accounts' balance within a single database transaction
 func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var result TransferTxResult
 
@@ -89,37 +88,10 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 			return err
 		}
 
-		// if arg.FromAccountID < arg.ToAccountID {
-		// 	result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
-		// } else {
-		// 	result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
-		// }
+		// TODO: update accounts' balance
 
 		return nil
 	})
 
 	return result, err
-}
-
-func addMoney(
-	ctx context.Context,
-	q *Queries,
-	accountID1 int64,
-	amount1 int64,
-	accountID2 int64,
-	amount2 int64,
-) (account1 Account, account2 Account, err error) {
-	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-		ID:     accountID1,
-		Amount: amount1,
-	})
-	if err != nil {
-		return
-	}
-
-	account2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-		ID:     accountID2,
-		Amount: amount2,
-	})
-	return
 }
