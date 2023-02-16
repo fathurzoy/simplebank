4d0a2ac966e8f85ada300a17ebbd3117abe6bbea
fathur rahman fauzan <fathurrahmanf123@gmail.com>
2023-02-16 16:07:05 +0700

    12-env-var-viper

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,20 +6,18 @@ import (
 	"os"
 	"testing"
 
+	"github.com/fathurzoy/util"
 	_ "github.com/lib/pq"
 )
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5433/simple_bank?sslmode=disable"
-)
-
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("cannot conncet to db", err)
