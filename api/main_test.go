6e4374c675b32c64a494fe8a62847d294fb214ce
fathur rahman fauzan <fathurrahmanf123@gmail.com>
2023-02-25 12:28:21 +0700

    21-implement-jwt-or-paseto

--- api/main_test.go
+++ api/main_test.go
@@ -1,11 +1,42 @@
 package api
 
 import (
+	"fmt"
+	"net/http"
 	"os"
 	"testing"
+	"time"
 
+	db "github.com/fathurzoy/simplebank/db/sqlc"
+	"github.com/fathurzoy/simplebank/token"
+	"github.com/fathurzoy/simplebank/util"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server{
+	config := util.Config{
+		TokenSymmetricKey: util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+	return server
+}
+
+
+func createAndSetAuthToken(t *testing.T, request *http.Request, tokenMaker token.Maker, username string) {
+	if len(username) == 0 {
+		return
+	}
+
+	token, err := tokenMaker.CreateToken(username, time.Minute)
+	require.NoError(t, err)
+
+	authorizationHeader := fmt.Sprintf("%s %s", authorizationTypeBearer, token)
+	request.Header.Set(authorizationHeaderKey, authorizationHeader)
+}
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
